00b6ddff33486781ba6e3b6ba67e8f8216874723
hamzaelbellaj <hamzaelbellaj@gmail.com>
2024-08-14 16:32:23 +0100

    ip

--- scrum-frontend/src/redux/actions/authActions.js
+++ scrum-frontend/src/redux/actions/authActions.js
@@ -3,7 +3,7 @@ import { LOGIN_SUCCESS, REGISTER_SUCCESS, REGISTER_FAILURE } from './types';
 
 export const login = (credentials) => async (dispatch) => {
   try {
-    const res = await axios.post('http://localhost:8080/api/auth/login', credentials);
+    const res = await axios.post('http://20.164.48.110:8080/api/auth/login', credentials);
     const { token, roles, userId } = res.data; // Récupérez l'ID
     const userRole = roles[0].name;
 
@@ -25,7 +25,7 @@ export const login = (credentials) => async (dispatch) => {
 
 export const register = (credentials) => async (dispatch) => {
   try {
-    const res = await axios.post('http://localhost:8080/api/auth/register', credentials);
+    const res = await axios.post('http://20.164.48.110:8080/api/auth/register', credentials);
     const { token, roles } = res.data;
     const userRole = roles[0].name; // Assurez-vous que roles n'est pas vide
 
